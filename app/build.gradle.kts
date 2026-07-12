778282e1e6456c04ef6a1adf9ea2c5411b1afcaa
dongpil kwak <support@appsteria.com.au>
2026-07-13 01:13:23 +1000

    fastlane

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.example.offlinegpt"
         minSdk = 25
         targetSdk = 37
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
